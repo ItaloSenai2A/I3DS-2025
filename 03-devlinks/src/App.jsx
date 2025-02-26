5b908af8344c2e707e90677a301dbfe6facd4859
Ítalo <italooo.senai@gmail.com>
2025-02-26 12:01:06 -0300

    Estrutura react vite completa

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,30 +1,52 @@
 
 import "./App.css"
+import foto from "./img/avatar.webp"
 
 import Perfil from './components/perfil/Perfil';
 import Switch from './components/switch/Switch';
 import Links from "./components/links/link";
 import SocialLinks from "./components/social/SocialLinks";
 import Rodape from "./components/rodape/Rodape";
+import { useState } from "react";
 
 const App = () => {
+  const [isLight,setIsLight] = useState(true)
+
+  const troca = () => {
+    setIsLight(!isLight);  
+  }
+
   return (
-    <div id="App" className="">
-    <Perfil/>
-    <Switch />
+    <div id="App" className={isLight && "light"}>
+
+    <Perfil fotoPerfil={foto}>
+      @ÍtaloFrancesco
+    </Perfil>
+    <Switch troca={troca } isLight={isLight}/>
     <ul>
-    <Links /> 
-    <Links /> 
-    <Links /> 
-    <Links /> 
+    <Links link={"https://github.com/ItaloSenai2A"}>GitHub</Links>
+    <Links link={"https://instagram.com"}>Instagram</Links>
+    <Links link={"https://github.com/ItaloSenai2A"}>Portifólio</Links>
+    <Links link={"https://github.com/ItaloSenai2A"}>Projetos</Links>
+
     </ul>
     <div id="socialLinks">
-<SocialLinks />
-<SocialLinks />
-<SocialLinks />
-<SocialLinks />
+<SocialLinks
+link={"https://github.com/ItaloSenai2A"} 
+icon={"logo-github"}
+/>
+<SocialLinks
+link={"https://instagram.com"} 
+icon={"logo-instagram"}
+ />
+<SocialLinks 
+link={"https://youtube.com"} 
+icon={"logo-youtube"}/>
+<SocialLinks 
+link={"https://linkedin.com"} 
+icon={"logo-linkedin"}/>
     </div>
-    <Rodape />
+    <Rodape>ÍtaloFrancesco</Rodape>
     </div>
     
     
--- 03-devlinks/src/components/rodape/Rodape.jsx
+++ 03-devlinks/src/components/rodape/Rodape.jsx
@@ -1,10 +1,10 @@
 import styles from './Rodape.module.css'
 
-const Rodape = () => {
+const Rodape = ({children}) => {
   return (
    <footer>
     <p>
-        Feito com 🩵 por <a href="https://github.com">MeuNome</a>
+        Feito com 🩵 por <a href="https://github.com">{children}</a>
     </p>
    </footer>
   )
